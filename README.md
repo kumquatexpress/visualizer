250956352d8f9d4ec6ad364567578b238f43976a
Boyang niu <boyang.niu@gmail.com>
2014-01-25 01:38:28 -0500

    hexagons created, moving on window resize and centering

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 Rhythm-based bullet hell game
 =============================
 
-This is what it sounds like.
+This is what it sounds like.  The hexagons on the index page are taken from James Tauber's CSS hexagon tutorial at http://jtauber.github.io/articles/css-hexagon.html.
--- scripts/grapher.js
+++ scripts/grapher.js
@@ -2,4 +2,11 @@ $(document).ready(function(){
     $("#run").click(function(){
         analyze_track();
     });
+
+    $(window).resize(function(){
+        var height = $(window).innerHeight();
+        var width = $(window).innerWidth();
+        $(".hex-holder").css("top", height/2+"px")
+            .css("left", width/2+"px");
+    });
 });
